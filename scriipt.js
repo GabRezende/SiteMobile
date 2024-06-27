94a00daeba11220cf77ecad32864f00e68b5a65c
unknown <contatogabrezende@gmail.com>
2024-06-27 15:29:13 -0300

    primeira versão incompleta

---
+++ scriipt.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('.slide').forEach(slide => {
+    slide.addEventListener('click', function(event) {
+        const description = this.querySelector('.description');
+        
+        // Mostra a descrição
+        if (!description.classList.contains('visible')) {
+            description.classList.remove('hidden');
+            description.classList.add('visible');
+        } else {
+            // Esconde a descrição
+            description.classList.remove('visible');
+            description.classList.add('hidden');
+        }
+        
+        // Evita que o evento de clique se propague para a imagem
+        event.stopPropagation();
+    });
+});
